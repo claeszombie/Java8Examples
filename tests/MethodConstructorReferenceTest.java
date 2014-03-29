b2779973167f31ea7c0c3073224262e5c17d1a32
Claes Persson <claes.zero@gmail.com>
2014-03-29 08:50:39 +0100

    Added reference Test

---
+++ tests/MethodConstructorReferenceTest.java
@@ -0,0 +1,45 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.util.function.Function;
+
+/**
+ * Created by claesdc on 2014-03-29.
+ */
+@RunWith(JUnit4.class)
+public class MethodConstructorReferenceTest {
+
+    class Zombie {
+        String firstName;
+        String lastName;
+
+        Zombie() {}
+
+        Zombie(String firstName, String lastName) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+    }
+    interface ZombieFactory<P extends Zombie> {
+        P create(String firstName, String lastName);
+    }
+
+    @Test
+    public void shouldUseMethodReference(){
+        Function<String,Integer> reference =  Integer::valueOf;
+        Integer converted = reference.apply("123");
+        Assert.assertNotNull(converted);
+        Assert.assertSame(123,converted);
+
+    }
+    @Test
+    public void shouldUseConstructorReference(){
+        //reference to the Zombie constructor via Zombie::new
+        ZombieFactory<Zombie> zombiefactory = Zombie::new;
+        //automatically chooses the right constructor by matching the signature of
+        Zombie zombie = zombiefactory.create("Daryl", "Dixon");
+        Assert.assertNotNull(zombie);
+    }
+}

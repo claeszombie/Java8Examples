a97af943eb5cf09658b1ce677bbcd0c67eb29007
Claes Persson <claes.zero@gmail.com>
2014-03-30 21:58:33 +0200

    added Predicate methods

--- tests/PredicateTest.java
+++ tests/PredicateTest.java
@@ -3,6 +3,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import java.util.Objects;
 import java.util.function.Predicate;
 
 import static junit.framework.Assert.assertEquals;
@@ -15,6 +16,12 @@ public class PredicateTest {
         private Predicate<String> predicate = (s) -> s.length() > 3;
         private Predicate<String> predicate2 = (s) -> s.charAt(0) == 'O';
         private Predicate<String> predicate3 = (s) -> s.charAt(0) == 'B';
+
+        private Predicate<Boolean> nonNull = Objects::nonNull;
+        private Predicate<Boolean> isNull = Objects::isNull;
+
+        private Predicate<String> isEmpty = String::isEmpty;
+        private Predicate<String> isNotEmpty = isEmpty.negate();
 
     @Before
     public void setUp(){
@@ -42,6 +49,32 @@ public class PredicateTest {
         assertEquals(true,predicate2.or(predicate3).test("Bka"));
         assertEquals(false,predicate2.or(predicate3).test("oka"));
         assertEquals(false,predicate2.or(predicate3).test("okay"));
+    }
+
+    @Test
+    public void predicateNonNullshouldWork(){
+        Boolean bisnull = null;
+        assertEquals(false,nonNull.test(bisnull));
+        assertEquals(true,nonNull.test(Boolean.FALSE));
+    }
+
+    @Test
+    public void predicateIsNullshouldWork(){
+        Boolean bisnull = null;
+        assertEquals(true,isNull.test(bisnull));
+        assertEquals(false,isNull.test(Boolean.FALSE));
+    }
+
+    @Test
+    public void predicateIsEmpty(){
+        assertEquals(true,isEmpty.test(""));
+        assertEquals(false,isEmpty.test("zombie"));
+    }
+    @Test
+    public void predicateIsNotEmpty(){
+        assertEquals(true,isNotEmpty.test("zombie"));
+        assertEquals(false,isNotEmpty.test(""));
+
     }
 
 }

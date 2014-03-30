0f490bb8342afa8a15384170b6b967911f034e18
Claes Persson <claes.zero@gmail.com>
2014-03-30 20:16:24 +0200

    added tests

--- tests/LambdaScopeTest.java
+++ tests/LambdaScopeTest.java
@@ -1,3 +1,5 @@
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -12,10 +14,48 @@ import static org.junit.Assert.assertEquals;
 @RunWith(JUnit4.class)
 public class LambdaScopeTest {
 
+    private static int outerStaticNum;
+    private int outerNum;
+
+    @Before
+    public void setUp(){
+        outerStaticNum = 0;
+    }
+
     @Test
     public void shouldAcessLocalVariabel(){
         int num = 1;
         Function<Integer,String> stringConverter = (from) -> String.valueOf(from + num);
         assertEquals("3", stringConverter.apply(2));
+    }
+
+    @Test
+    public void shouldAccesStaticVariable(){
+        Assert.assertEquals(0,outerStaticNum);
+
+        Function<Integer,String> stringconverter = (from) -> {
+            outerStaticNum = 72;
+            return String.valueOf(from);
+        };
+
+        String value = stringconverter.apply(1);
+
+        Assert.assertEquals("1", value);
+        Assert.assertEquals(72, outerStaticNum);
+    }
+
+    @Test
+    public void shouldAccessInstanceField(){
+        Assert.assertEquals(0,outerNum);
+
+        Function<Integer,String> stringconverter = (from) -> {
+            outerNum = 72;
+            return String.valueOf(from);
+        };
+
+        String value = stringconverter.apply(1);
+
+        Assert.assertEquals("1", value);
+        Assert.assertEquals(72, outerNum);
     }
 }

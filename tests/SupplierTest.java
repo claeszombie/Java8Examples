4a05b80e7b94eb98386dc56e35b351065169bbc0
Claes Persson <claes.zero@gmail.com>
2014-04-02 22:25:42 +0200

    added Supplier test

--- tests/SupplierTest.java
+++ tests/SupplierTest.java
@@ -1,9 +1,22 @@
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+
+import java.util.Date;
+import java.util.function.Supplier;
 
 /**
  *
  */
 @RunWith(JUnit4.class)
 public class SupplierTest {
+
+    @Test
+    public void supplierShouldGetResult(){
+        Supplier<Date> stringSupplier = Date::new;
+        Date datesupplied = stringSupplier.get();
+
+        Assert.assertNotNull(datesupplied);
+    }
 }

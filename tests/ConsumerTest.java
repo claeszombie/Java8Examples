29c95e9d74df9fe4e3664a42d54c4f2559fe839f
Claes Persson <claes.zero@gmail.com>
2014-04-02 22:25:29 +0200

    added Consumer test

--- tests/ConsumerTest.java
+++ tests/ConsumerTest.java
@@ -1,5 +1,23 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.time.Clock;
+import java.time.Instant;
+import java.util.function.Consumer;
+
 /**
  *
  */
+@RunWith(JUnit4.class)
 public class ConsumerTest {
+
+    @Test
+    public void consumerShouldAcceptArgument(){
+        Consumer<Instant> apocalypsetime = (ap) -> System.out.println("It's time "+ ap );
+        apocalypsetime.accept(Clock.systemDefaultZone().instant());
+
+        Assert.assertNotNull(apocalypsetime);
+    }
 }
